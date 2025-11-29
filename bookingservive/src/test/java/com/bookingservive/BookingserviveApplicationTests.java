a93439977a5b7860e3ce1c15fb930bf7c53ed4c9
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-11-29 19:44:17 +0530

    Initial Setup

---
+++ bookingservive/src/test/java/com/bookingservive/BookingserviveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingservive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingserviveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
